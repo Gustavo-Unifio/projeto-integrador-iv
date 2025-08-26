0af184ac4a9e106d6188e6d3567089e0eb757d41
Gustavo-Unifio <272164@unifio.edu.br>
2025-08-26 23:10:47 +0000

    Atividade da lista de aluno e status

--- Teste/src/app/estrutura/diretiva/diretiva.ts
+++ Teste/src/app/estrutura/diretiva/diretiva.ts
@@ -12,4 +12,20 @@ export class Diretiva {
   toggleOcultar() {
     this.mostrarOcultar = !this.mostrarOcultar;
   }
+
+  listaAluno = [
+    { id: '101', nome: 'Thiago', ativo: true },
+    { id: '102', nome: 'Sergio', ativo: false },
+    { id: '103', nome: 'Marinke', ativo: true }
+  ];
+
+  fontSize: number = 16;
+
+  maisFonte(){
+    this.fontSize += 2;
+  }
+
+  menosFonte(){
+    this.fontSize -= 2;
+  }
 }
